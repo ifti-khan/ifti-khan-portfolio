a85b4e73e9eaf8bdde0dec07b94db1e103250318
ifti-khan <68855379+ifti-khan@users.noreply.github.com>
2020-12-08 22:23:54 +0000

    bug: fixed contact form js,  fix: contact form button for mobile

--- assets/js/send_email.js
+++ assets/js/send_email.js
@@ -9,10 +9,16 @@ document.getElementById('contactform')
         const serviceID = 'ifti-khan-portfolio';
         const templateID = 'iftikhan-email-template';
 
-        emailjs.sendForm(serviceID, templateID, this)
-            .then(() => {
+        emailjs.send(serviceID, templateID, {
+                "from_name": this.fullname.value,
+                "message": this.message.value,
+                "subject": this.subject.value,
+                "from_email": this.emailaddress.value,
+                "contact_number": this.number.value,
+            })
+            .then((response) => {
                 btn.textContent = 'Send';
-                console.log("Message Has Been Sent");
+                console.log("Message Has Been Sent", response);
                 if (alert('Your message has been sent!')) {} else window.location.reload();
             }, (error) => {
                 btn.textContent = 'Send';
@@ -22,7 +28,9 @@ document.getElementById('contactform')
             });
     });
 
-/*function sendMail(contactForm) {
+
+/*old code but works fine
+function sendMail(contactForm) {
     emailjs.send("ifti-khan-portfolio", "iftikhan-email-template", {
             "from_name": contactForm.fullname.value,
             "message": contactForm.message.value,
@@ -34,11 +42,6 @@ document.getElementById('contactform')
             function (response) {
                 console.log("SUCCESS", response);
 
-                Confirm message
-                if (confirm('Successful Message')) {
-                    window.location.reload();
-                }
-
                 if (alert('Your message has been sent!')) {} else window.location.reload();
 
 
@@ -46,11 +49,18 @@ document.getElementById('contactform')
             function (error) {
                 console.log("FAILED", error);
                 if (alert(`Oops something went wrong!`, error)) {} else
-
-                    href redirect but allow user to go back
-                    window.location.href = "assets/pages/message_404.html";
-                    window.location.replace("assets/pages/message_404.html");
+                window.location.replace("assets/pages/message_404.html");
             }
         );
     return false; // To block from loading a new page
 }*/
+
+/*
+Confirm message
+if (confirm('Successful Message')) {
+    window.location.reload();
+}
+
+ href redirect but allow user to go back
+                    window.location.href = "assets/pages/message_404.html";
+*/
